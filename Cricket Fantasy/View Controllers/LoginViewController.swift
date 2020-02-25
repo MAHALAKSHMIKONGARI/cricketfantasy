81665c1705c8e2fd973157bd3dcdfa2923fb9214
pinky407 <54380650+pinky407@users.noreply.github.com>
2020-02-24 19:54:52 -0600

    Login view controller

---
+++ Cricket Fantasy/View Controllers/LoginViewController.swift
@@ -0,0 +1,77 @@
+//
+//  LoginViewController.swift
+//  Cricket Fantasy
+//
+//  Created by student on 2/19/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.jpg")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func LoginTapped(_ sender: Any) {
+        
+        let email = emailTF.text!
+        let password = passwordTF.text!
+        
+        
+        //Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil{
+                
+                let ac = UIAlertController(title: "not matching", message: "try again", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .cancel)
+                ac.addAction(action)
+                self.present(ac, animated: true)
+            }
+            else{
+                
+                 self.tansitionToHome()
+                
+            }
+        }
+    }
+    
+    func tansitionToHome(){
+        
+       
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
+    
+    
+    
+
+}
