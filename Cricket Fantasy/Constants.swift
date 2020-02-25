d94570c45f17c12592ccffeb5383fc0d3b440e24
Maha Lakshmi Kongari <mahalakshmi.kongari@gmail.com>
2020-02-24 19:46:09 -0600

    initial commit
    First commit after project creation

---
+++ Cricket Fantasy/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Cricket Fantasy
+//
+//  Created by student on 2/20/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Constants{
+    
+    struct Storyboard{
+        
+        static let homeViewController = "HomeVc"
+    }
+}
---
+++ Cricket Fantasy/View Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Cricket Fantasy
+//
+//  Created by student on 2/19/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginButton: UIButton!
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.jpg")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+    }
+
+
+}
+
