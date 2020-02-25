dccefb5711d70657e017d938f8a8d82d89809235
pinky407 <54380650+pinky407@users.noreply.github.com>
2020-02-24 19:54:39 -0600

    Home view controller

---
+++ Cricket Fantasy/View Controllers/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  Cricket Fantasy
+//
+//  Created by student on 2/19/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.jpg")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+    }
+    
+    
+    
+    
+    
+    
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
